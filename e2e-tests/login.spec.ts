565f531b7e3bb3e74ee4398c572894a45d828fc8
BomScoob12 <sorawitkraukham@gmail.com>
2024-11-18 14:54:36 +0700

    init

---
+++ e2e-tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('login test', async ({ page }) => {
+  await page.goto('https://workshop-playwright.vercel.app/');
+  await page.getByTestId('username-input').fill('BomScoob');
+  await page.getByTestId('password-input').fill('password');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Welcome To ODDS| Conference Playwright Workshop')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Log out' })).toBeVisible();
+});
+
+test('Login failed test', async ({page}) => {
+    await page.goto('https://workshop-playwright.vercel.app/');
+    await page.getByTestId('username-input').fill('BomScoob');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByText('Password is required')).toBeVisible();
+});
