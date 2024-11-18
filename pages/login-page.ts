fa05cba3e5f772c0c85d82061f5e78dca1cf71e5
BomScoob12 <sorawitkraukham@gmail.com>
2024-11-18 15:37:08 +0700

    refactor: Update login-page.ts
    - Refactor the login-page.ts file to improve code readability and maintainability.
    - Update the URL constant to use a class property for easier modification.
    - Replace deprecated methods with updated Playwright methods for filling input fields.
    - Add a new method to display the login page and verify the title.

--- pages/login-page.ts
+++ pages/login-page.ts
@@ -1,25 +1,38 @@
 import { Page, test, expect } from '@playwright/test';
 
 class LoginPage {
-    readonly page: Page;
+  readonly page: Page;
+  readonly URL = 'https://workshop-playwright.vercel.app/';
 
-    async goToLoginPage() {
-        await this.page.goto('https://workshop-playwright.vercel.app/');
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    async fillUsername(username: string = "BomScoob") {
-        await this.page.fill('[data-testid=username-input]', username);
-    }
+  async goToLoginPage() {
+    await this.page.goto(this.URL);
+  }
 
-    async fillPassword(password: string = "password") {
-        await this.page.fill('[data-testid=password-input]', password);
-    }
+  async fillUsername(username: string) {
+    await this.page.getByTestId('username-input').fill(username);
+  }
 
-    async clickLoginButton() {
-        await this.page.getByRole('button', { name: 'Login'}).click();
-    }
+  async fillPassword(password: string) {
+    await this.page.getByTestId('password-input').fill(password);
+  }
 
-    async clickLogoutButton() {
-        await this.page.getByRole('button', { name: 'Log out'}).click();
-    }
+  async clickLoginButton() {
+    await this.page.getByRole('button', { name: 'Login' }).click();
+  }
+
+  async clickLogoutButton() {
+    await this.page.getByRole('button', { name: 'Log out' }).click();
+  }
+
+  async displayLoginPage() {
+    await expect(this.page.getByTestId('login-title')).toContainText(
+      'Welcome To ODDS| Conference'
+    );
+  }
 }
+
+export default LoginPage;
